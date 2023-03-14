b678e974fab9401ffa2cada959689f5bf1a6652c
MarTinBarrio <barrio_m@yahoo.com.ar>
2023-03-14 11:47:51 -0300

    final py

--- App.js
+++ App.js
@@ -2,66 +2,80 @@ import { StyleSheet, ImageBackground, SafeAreaView } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
-import { useFonts } from 'expo-font'; //https://docs.expo.dev/versions/latest/sdk/font/ (se pueden agregar google fonts)
+import { useFonts } from "expo-font"; //https://docs.expo.dev/versions/latest/sdk/font/ (se pueden agregar google fonts)
 import AppLoading from "expo-app-loading"; //https://www.npmjs.com/package/expo-app-loading (se usa para dmorar start, mientras carga los fonts, img, etc)
+
+import { StatusBar } from "expo-status-bar";
 
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 import Colors from "./constants/colors";
 
 export default function App() {
-
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
   const [guessRounds, setGetRounds] = useState(0);
 
-  const [fontloaded] = useFonts ({ //useFonts devuelve un array, el 1er elemento es boolean indicando si terminó la carga o nó
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
-  })
+  const [fontloaded] = useFonts({
+    //useFonts devuelve un array, el 1er elemento es boolean indicando si terminó la carga o nó
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
 
-  if(!fontloaded){
+  if (!fontloaded) {
     return <AppLoading />;
   }
 
-  function pickedNumberHandler(pickedNumber){
-    setUserNumber (pickedNumber);
-    setGameIsOver (false);
+  function pickedNumberHandler(pickedNumber) {
+    setUserNumber(pickedNumber);
+    setGameIsOver(false);
   }
 
-  function gameOverHandler (rounds) {
+  function gameOverHandler(rounds) {
     setGameIsOver(true);
     //setGetRounds(rounds);
   }
 
-  function starNweGameHandler(){
+  function starNweGameHandler() {
     setUserNumber(null);
     setGetRounds(0);
   }
 
-  let screen = <StartGameScreen onPickNumber={pickedNumberHandler}/>;
+  let screen = <StartGameScreen onPickNumber={pickedNumberHandler} />;
 
-  if (userNumber){
-    screen = < GameScreen userNumber={userNumber} onGameOver={gameOverHandler}/>;
+  if (userNumber) {
+    screen = (
+      <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />
+    );
   }
 
-  if (gameIsOver && userNumber){
-    screen = < GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onStarNewGame={starNweGameHandler}/>;
+  if (gameIsOver && userNumber) {
+    screen = (
+      <GameOverScreen
+        roundsNumber={guessRounds}
+        userNumber={userNumber}
+        onStarNewGame={starNweGameHandler}
+      />
+    );
   }
-
-  
 
   return (
-    <LinearGradient colors={[Colors.primary700, Colors.accent500]} style={styles.rootScreen}>
-      <ImageBackground
-        source={require('./assets/images/background.png')}
-        resizeMode="cover"
+    <>
+      <StatusBar style='light'/>
+      <LinearGradient
+        colors={[Colors.primary700, Colors.accent500]}
         style={styles.rootScreen}
-        imageStyle = {styles.backgroundImage}
       >
-        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
-      </ImageBackground>
-    </LinearGradient>
+        <ImageBackground
+          source={require("./assets/images/background.png")}
+          resizeMode="cover"
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+        >
+          <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+        </ImageBackground>
+      </LinearGradient>
+    </>
   );
 }
 
@@ -71,5 +85,5 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     opacity: 0.15,
-  }
+  },
 });
